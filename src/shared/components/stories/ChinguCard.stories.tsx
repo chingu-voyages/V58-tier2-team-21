8e77b9c252e01afca5547ad907b20f6bda55ff9a
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-19 12:40:04 +0100

    feat: create stories for ChinguCard

---
+++ src/shared/components/stories/ChinguCard.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj as Story } from "@storybook/react";
+import ChinguCard from "../ChinguCard";
+
+const meta = {
+  title: "Components/ChinguCard",
+  component: ChinguCard,
+  tags: ["autodocs"],
+  args: {
+    gender: "MALE",
+    country: "New Zealand",
+    roleType: "Web",
+    voyageRole: "Developer",
+    soloProjectTier: "Tier 2",
+    voyageTier: "Tier 2",
+    voyageNum: "V58",
+    timestamp: "2025-10-18 08:56",
+  },
+} satisfies Meta<typeof ChinguCard>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const EmptyTiers: Story = {
+  args: {
+    soloProjectTier: "",
+    voyageTier: "",
+    voyageNum: "",
+  },
+};
+
+export const DifferentRole: Story = {
+  args: {
+    roleType: "Python",
+    voyageRole: "Product Owner",
+  },
+};
+
+export const DifferentCountry: Story = {
+  args: {
+    country: "Canada",
+  },
+};
+
+export default meta;
