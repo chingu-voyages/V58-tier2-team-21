d4f08b8515b1ac3bc5949734703c2f9b64e703ec
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-19 18:36:39 +0100

    chore: update styling of ChinguCard

--- src/shared/components/ChinguCard.tsx
+++ src/shared/components/ChinguCard.tsx
@@ -1,38 +1,49 @@
 type ChinguCardPropsType = {
-  gender: string,
-  countryName: string,
-  countryCode: string,
-  roleType: string,
-  voyageRole: string,
-  soloProjectTier: string,
-  voyageTier: string,
-  voyageNum: string,
-  timestamp: string
-}
+  gender: string;
+  countryName: string;
+  countryCode: string;
+  roleType: string;
+  voyageRole: string;
+  soloProjectTier: string;
+  voyageTier: string;
+  voyageNum: string;
+  timestamp: string;
+};
 
 export default function ChinguCard(props: ChinguCardPropsType) {
- return (
-  <>
-    <div className="card border border-black rounded-lg shadow-md p-2 flex flex-row items-center w-max gap-2">
-      <img className="flag-avatar w-10 h-full shrink-0" src={`https://flagcdn.com/w40/${props.countryCode.toLowerCase()}.png`} alt="flag-avatar" />
-      <div className="content flex flex-col justify-center gap-1">
+  return (
+    <>
+      <div className="card bg-gray-200 border border-black rounded-lg shadow-md p-2 flex flex-row items-center w-max gap-2">
+        <img
+          className="flag-avatar shrink-0 rounded-full border border-gray-300 shadow-lg"
+          src={`https://flagcdn.com/w80/${props.countryCode.toLowerCase()}.jpg`}
+          alt="flag-avatar"
+        />
+        <div className="content flex flex-col justify-center gap-1">
+          <div className="line-1 flex flex-row gap-2 divide-x-2 just items-center">
+            <span className="country pr-2">{props.countryName}</span>
+            <span className="year pr-2">Joined in: 2025</span>
+            <span className="gender">{props.gender.toLowerCase()}</span>
+          </div>
 
-        <div className="line-1 flex flex-row gap-2 items-center">
-          <span className="country">{props.countryName}</span>
-          <span className="year">2025</span>
-          <span className="gender">{props.gender}</span>
+          <div className="line-2 flex flex-row gap-2 divide-x-2 items-center">
+            <span className="role pr-2">
+              {props.roleType} {props.voyageRole}
+            </span>
+            <span className="solo-tier pr-2">
+              Solo tier:{" "}
+              {props.soloProjectTier.length > 0 ? props.soloProjectTier : "-"}
+            </span>
+            <span className="voyage-tier pr-2">
+              Voyage tier:{" "}
+              {props.voyageTier.length > 0 ? props.voyageTier : "-"}
+            </span>
+            <span className="voyage-num">
+              Voyage: {props.voyageNum.length > 0 ? props.voyageNum : "-"}
+            </span>
+          </div>
         </div>
-
-        <div className="line-2 flex flex-row gap-2 items-center">
-          <span className="role">{props.roleType} {props.voyageRole}</span>
-          <span className="solo-tier"> Solo tier: {props.soloProjectTier.length > 0 ? props.soloProjectTier : "-"}</span>
-          <span className="voyage-tier">Voyage tier: {props.voyageTier.length > 0 ? props.voyageTier : "-"}</span>
-          <span className="voyage-num">Voyage: {props.voyageNum.length > 0 ? props.voyageNum : "-"}</span>
-        </div>
-
       </div>
-    </div>
-  </>
- )
-
+    </>
+  );
 }
--- src/shared/components/stories/ChinguCard.stories.tsx
+++ src/shared/components/stories/ChinguCard.stories.tsx
@@ -39,7 +39,8 @@ export const DifferentRole: Story = {
 
 export const DifferentCountry: Story = {
   args: {
-    country: "Canada",
+    countryName: "Canada",
+    countryCode: "CA"
   },
 };
 
