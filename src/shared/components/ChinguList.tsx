8128a15f110c32f41467469193075e69f7c64db8
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-20 11:25:16 +0100

    chore: update styling of ChinguList

--- src/shared/components/ChinguList.tsx
+++ src/shared/components/ChinguList.tsx
@@ -2,9 +2,11 @@ import ChinguCard from "./ChinguCard";
 import type { ChinguCardPropsType } from "./ChinguCard";
 
 export default function ChinguList(props: { data: ChinguCardPropsType[] }) {
-  return props.data.map((chingu, index) => (
-    <div key={index} className="grid-cols-2 gap-2">
-      <ChinguCard {...chingu} />
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      {props.data.map((chingu, index) => (
+        <ChinguCard key={index} {...chingu} />
+      ))}
     </div>
-  ));
+  );
 }
