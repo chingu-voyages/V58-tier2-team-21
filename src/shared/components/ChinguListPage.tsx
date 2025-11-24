10a5b4865c3019b668ff57b440bc43a1ec5e6282
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-24 11:51:08 +0100

    chore: connect ChinguSearch to ChinguListPage state

--- src/shared/components/ChinguListPage.tsx
+++ src/shared/components/ChinguListPage.tsx
@@ -2,16 +2,20 @@ import ChinguFilter from "./ChinguFilter";
 import ChinguList from "./ChinguList";
 import type { ChinguCardPropsType } from "./ChinguCard";
 import ChingueSearch from "./ChinguSearch";
+import { useState } from "react";
 
 type ChinguListPageProps = {
   data: ChinguCardPropsType[];
 }
 
 export default function ChinguListPage({ data }: ChinguListPageProps) {
+  
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  
   return (
     <div className="flex flex-row gap-4">
       <div className="md:w-1/3 lg:w-1/4 bg-gray-200 border border-gray-400 rounded-lg p-2">
-        <ChingueSearch />
+        <ChingueSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <ChinguFilter />
       </div>
       <div className="flex-1">
--- src/shared/components/ChinguSearch.tsx
+++ src/shared/components/ChinguSearch.tsx
@@ -1,13 +1,23 @@
-export default function ChingueSearch() {
+type ChinguSearchProps = {
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function ChingueSearch({
+  searchTerm,
+  setSearchTerm,
+}: ChinguSearchProps) {
   return (
     <div>
-       <input
-          type="search"
-          id="chingu-search"
-          placeholder="Keywords come here"
-          className="border border-gray-400 p-1 rounded-lg mb-2 mr-2"
-        />
-        <button type="submit">Search</button>
+      <input
+        type="text"
+        id="chingu-search"
+        placeholder="Keywords come here"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="border border-gray-400 p-1 rounded-lg mb-2 mr-2"
+      />
+      <button type="submit">Search</button>
     </div>
-  )
+  );
 }
