747db6e74795fcc07686cf434cf3150954041e25
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-24 11:27:59 +0100

    chore: ChinguSearch added to ChinguListPage

--- src/shared/components/ChinguListPage.tsx
+++ src/shared/components/ChinguListPage.tsx
@@ -1,6 +1,7 @@
 import ChinguFilter from "./ChinguFilter";
 import ChinguList from "./ChinguList";
 import type { ChinguCardPropsType } from "./ChinguCard";
+import ChingueSearch from "./ChinguSearch";
 
 type ChinguListPageProps = {
   data: ChinguCardPropsType[];
@@ -10,6 +11,7 @@ export default function ChinguListPage({ data }: ChinguListPageProps) {
   return (
     <div className="flex flex-row gap-4">
       <div className="md:w-1/3 lg:w-1/4 bg-gray-200 border border-gray-400 rounded-lg p-2">
+        <ChingueSearch />
         <ChinguFilter />
       </div>
       <div className="flex-1">
