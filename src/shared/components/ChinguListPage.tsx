71283ecbc5d3f1a2e105f5c10d2235f0005ed99d
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-11-24 11:20:13 +0100

    chore: update mockData for testing

--- src/shared/components/ChinguListPage.tsx
+++ src/shared/components/ChinguListPage.tsx
@@ -8,8 +8,8 @@ type ChinguListPageProps = {
 
 export default function ChinguListPage({ data }: ChinguListPageProps) {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="md:w-1/3 lg:w-1/4">
+    <div className="flex flex-row gap-4">
+      <div className="md:w-1/3 lg:w-1/4 bg-gray-200 border border-gray-400 rounded-lg p-2">
         <ChinguFilter />
       </div>
       <div className="flex-1">
