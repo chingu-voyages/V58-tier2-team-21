b73c0544feee859ce2af9cb5c0c6917f2997291d
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-12-01 11:51:17 +0100

    chore: chinguData added + ReactRouter fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-
-import router from "./routes";
-
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
