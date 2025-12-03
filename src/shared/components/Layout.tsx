a5b68ba7346d7db03eaaf66268c09f642f16c126
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-12-03 13:35:01 +0100

    chore: update Storybook preview

--- src/shared/components/Layout.tsx
+++ src/shared/components/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
 
-export default function Layout() {
+export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="layout">
       <main>
+        {children}
         <Outlet />
       </main>
     </div>
