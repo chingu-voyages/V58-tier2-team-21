86fe7518debed569e7e2c729fd0fb6934b622db2
BastienWinant <bastien.winant@gmail.com>
2025-12-03 14:18:27 +0100

    use component class for header styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,9 @@ import HeaderNav from "./HeaderNav.tsx";
 
 export default function Header() {
   return (
-    <header className="text-(--color-white-100) py-6 sm:py-8 relative">
+    <header className="text-(--color-white-100) py-6 sm:py-8">
       <Container>
-        <div className="flex justify-between">
+        <div className="header">
           <h1 className="font-bold text-xl">
             <span className="text-(--color-primary-light)">Chingu</span>
             <span className="text-(--color-secondary)">Demo</span>
--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -4,18 +4,15 @@ import CloseIcon from "../../assets/img/16-c-remove.svg";
 export default function HeaderNav() {
   return (
     <nav
-      className="fixed sm:static top-0 right-0 w-64 sm:w-auto h-full sm:h-auto bg-(--color-black-200) sm:bg-transparent px-8 py-24 sm:p-0"
+      className="header-nav"
+      // className="fixed sm:static top-0 right-0 w-64 sm:w-auto h-full sm:h-auto bg-(--color-black-200) sm:bg-transparent px-8 py-24 sm:p-0"
     >
       <button className="absolute top-6 right-6 sm:hidden">
         <img className="h-6 w-6" src={CloseIcon} alt="Close icon." />
       </button>
-      <ul
-        className="flex flex-col gap-y-4 sm:flex-row sm:items-center gap-x-8 md:gap-x-12 xl:gap-x-16"
-      >
-        <li><HeaderLink text="Home" to="/" /></li>
-        <li><HeaderLink text="List" to="list" /></li>
-        <li><HeaderLink text="Map" to="map" /></li>
-      </ul>
+      <HeaderLink text="Home" to="/" />
+      <HeaderLink text="List" to="list" />
+      <HeaderLink text="Map" to="map" />
     </nav>
   )
 }
