70d248bcd1e4881e6054a0f432c9d5cba006046d
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-12-04 14:12:03 +0100

    chore: improve responsiveness on big screens

--- src/shared/components/ChinguList.tsx
+++ src/shared/components/ChinguList.tsx
@@ -3,7 +3,7 @@ import type { ChinguCardPropsType } from "./ChinguCard";
 
 export default function ChinguList(props: { data: ChinguCardPropsType[] }) {
   return (
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3 px-2 sm:px-4 mx-auto">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 3xl:grid-cols-3 gap-3 px-2 sm:px-4 mx-auto justify-items-center">
       {props.data.map((chingu, index) => (
         <ChinguCard key={index} {...chingu} />
       ))}
