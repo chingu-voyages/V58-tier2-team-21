1cfa098fdb5fd3518c0a72b31d1df669c93b23a6
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-12-13 19:42:32 +0100

    chore: fix Submit button color on mobile screens

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,21 +1,24 @@
 import { type ButtonHTMLAttributes } from "react";
 
 type ButtonPropsType = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "filter";
 };
 
 export default function Button({
   variant,
+  className = "",
   children,
   ...props
 }: ButtonPropsType) {
   const classes =
     variant === "primary"
       ? "bg-primary-light text-backgroundLigher px-4 py-2 rounded hover:bg-primary hover:cursor-pointer disabled:opacity-50"
-      : "bg-black-100 text-white px-4 py-2 rounded hover:bg-gray-500 disabled:opacity-50 hover:cursor-pointer";
+      : variant === "secondary"
+        ? "bg-black-100 text-white px-4 py-2 rounded hover:bg-gray-500 disabled:opacity-50 hover:cursor-pointer"
+        : "bg-secondary-light text-backgroundLigher px-4 py-2 rounded hover:bg-primary hover:cursor-pointer disabled:opacity-50";
 
   return (
-    <button {...props} className={classes}>
+    <button {...props} className={`${classes} ${className}`}>
       {children}
     </button>
   );
